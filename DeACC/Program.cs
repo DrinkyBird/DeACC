11c3c735f74e9a2c4e9a7b36f14616a79af4bdb9
Sean Baggaley <sean@csnxs.uk>
2017-05-22 16:48:30 +0100

    Add command line stuff

--- DeACC/Program.cs
+++ DeACC/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 
 namespace Csnxs.DeACC
 {
     class Program
     {
+        #region Command Line Verbs
+
+
+        [Verb("disassemble", HelpText = "Disassemble an ACS file")]
+        class DisassembleOptions
+        {
+            [Value(0, MetaName = "file", HelpText = "Compiled ACS object file", Required = true)]
+            public long FileName { get; set; }
+        }
+
+        #endregion
+
         static void Main(string[] args)
         {
+            new Program(args);
+        }
+
+        private Program(string[] args)
+        {
+            int ret = CommandLine.Parser.Default.ParseArguments<DisassembleOptions>(args)
+                .MapResult(
+                    Disassemble,
+                errs => 1);
+        }
+
+        private int Disassemble(DisassembleOptions options)
+        {
+
+
+            return 0;
         }
     }
 }
