69e12855298abeefa376825ab2dae43f4dd9d53d
Sean Baggaley <sean@csnxs.uk>
2017-05-22 17:16:34 +0100

    Add a test file and ACS format identifier

---
+++ DeACC/ACSFormatIdentifier.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csnxs.DeACC
+{
+    enum AcsFormat
+    {
+        NotAcs,
+        Acs95,          // ACS\0
+        ZDoomUpper,     // ACSE
+        ZDoomLower      // ACSe
+    }
+
+    class AcsFormatIdentifier
+    {
+        // Disallow construction of this class.
+        private AcsFormatIdentifier() {}
+
+        public static AcsFormat IdentifyFormat(Stream stream)
+        {
+            long originalPos = stream.Position;
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            byte[] sig = new byte[4];
+            stream.Read(sig, 0, 4);
+            stream.Seek(originalPos, SeekOrigin.Begin);
+
+            if (sig[0] != 'A' || sig[1] != 'C' || sig[2] != 'S')
+            {
+                return AcsFormat.NotAcs;
+            }
+
+            if (sig[3] == 'e')
+            {
+                return AcsFormat.ZDoomLower;
+            }
+            else if (sig[3] == 'E')
+            {
+                return AcsFormat.ZDoomUpper;
+            }
+            else if (sig[3] == '\0')
+            {
+                stream.Seek(4, SeekOrigin.Begin);
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    int dirOffset = reader.ReadInt32();
+
+                    stream.Seek(dirOffset - 4, SeekOrigin.Begin);
+                    stream.Read(sig, 0, 4);
+                    stream.Seek(originalPos, SeekOrigin.Begin);
+
+                    if (sig[0] != 'A' || sig[1] != 'C' || sig[2] != 'S')
+                    {
+                        return AcsFormat.Acs95;
+                    }
+
+                    if (sig[3] == 'e')
+                    {
+                        return AcsFormat.ZDoomLower;
+                    }
+                    else if (sig[3] == 'E')
+                    {
+                        return AcsFormat.ZDoomUpper;
+                    }
+                }
+            }
+
+            return AcsFormat.NotAcs;
+        }
+    }
+}
--- DeACC/Program.cs
+++ DeACC/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace Csnxs.DeACC
         class DisassembleOptions
         {
             [Value(0, MetaName = "file", HelpText = "Compiled ACS object file", Required = true)]
-            public long FileName { get; set; }
+            public string FileName { get; set; }
         }
 
         #endregion
@@ -33,9 +34,26 @@ namespace Csnxs.DeACC
                 errs => 1);
         }
 
+        public static void PrintError(string line)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine("[ERROR] " + line);
+            Console.ResetColor();
+        }
+
         private int Disassemble(DisassembleOptions options)
         {
+            string path = options.FileName;
+            if (!File.Exists(path))
+            {
+                PrintError("File does not exist: " + path);
+                return 2;
+            }
 
+            FileStream stream = new FileStream(path, FileMode.Open);
+            AcsFormat format = AcsFormatIdentifier.IdentifyFormat(stream);
+
+            Console.WriteLine("Detected format: " + format);
 
             return 0;
         }
