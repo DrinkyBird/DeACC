82c3e55c9ed0c5f4099d210356d82146033e75ae
Sean Baggaley <sean@csnxs.uk>
2023-06-03 04:27:51 +0100

    Don't disassemble imported functions

--- DeACC/AcsFunction.cs
+++ DeACC/AcsFunction.cs
@@ -11,16 +11,20 @@ namespace Csnxs.DeACC
         public string Name;
         public int NumberOfArguments { get; private set; }
         public int NumberOfVariables { get; private set; }
+        public int ImportNum { get; private set; }
         public bool Returns { get; private set; }
         public int Pointer { get; private set; }
         public int CodeSize;
 
         public AcsInstruction[] Code;
 
-        public AcsFunction(int argc, int varc, bool returns, int ptr)
+        public bool IsImported => Pointer == 0;
+
+        public AcsFunction(int argc, int varc, bool returns, int importNum, int ptr)
         {
             NumberOfArguments = argc;
             NumberOfVariables = varc;
+            ImportNum = importNum;
             Returns = returns;
             Pointer = ptr;
         }
