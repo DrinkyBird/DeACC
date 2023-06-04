54112ddf97380ef83aecba0341a735f75535831c
Sean Baggaley <sean@csnxs.uk>
2023-06-04 03:20:02 +0100

    Add a couple tests for AcsFormatIdentifier

---
+++ DeACC.Test/AcsFormatIdentifierTest.cs
@@ -0,0 +1,24 @@
+﻿namespace DeACC.Test
+{
+    [TestClass]
+    public class AcsFormatIdentifierTest
+    {
+        [TestMethod]
+        [DeploymentItem("Data/hexen_map03.o")]
+        public void IdentifyAcs95()
+        {
+            using var stream = new FileStream("hexen_map03.o", FileMode.Open, FileAccess.Read);
+            var format = AcsFormatIdentifier.IdentifyFormat(stream);
+            Assert.AreEqual(format, AcsFormat.Acs95);
+        }
+
+        [TestMethod]
+        [DeploymentItem("Data/lib1.o")]
+        public void IdentifyAcsZDoomLower()
+        {
+            using var stream = new FileStream("lib1.o", FileMode.Open, FileAccess.Read);
+            var format = AcsFormatIdentifier.IdentifyFormat(stream);
+            Assert.AreEqual(format, AcsFormat.ZDoomLower);
+        }
+    }
+}
